3fabe2889174b7671f210aaee4b5b1235e006c00
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-15 02:04:11 +0100

    feat: Add maintenance configuration for site maintenance mode

---
+++ lib/maintenanceConfig.ts
@@ -0,0 +1,7 @@
+// Simple config for maintenance mode. Set to true to enable the banner.
+const maintenanceConfig = {
+  enabled: false,
+  message: "The site is currently undergoing maintenance. Some features may be unavailable."
+};
+
+export default maintenanceConfig;
